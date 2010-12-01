f3112f262e29c49e3898b580f6956f4efe0a51b5
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2010-12-02 01:00:23 +0300

    build: Minor skeleton builder refactoring

--- src/build/dae/skeletonbuilder.fs
+++ src/build/dae/skeletonbuilder.fs
@@ -7,23 +7,21 @@ open SlimDX
 open Build.Dae.Parse
 
 let private getNodeTransformComponent (comp: XmlNode) =
-    let data = lazy(comp.InnerText |> Build.Dae.Parse.splitWhitespace |> Array.map (fun s -> float32 s))
-    let d () = data.Force()
+    let data n = Build.Dae.Parse.parseFloatArray comp.InnerText n
 
     match comp.Name with
-    | "translate" -> Matrix.Translation(d().[0], d().[1], d().[2])
-    | "rotate" -> Matrix.RotationAxis(Vector3(d().[0], d().[1], d().[2]), d().[3] / 180.0f * float32 System.Math.PI)
-    | "scale" -> Matrix.Scaling(d().[0], d().[1], d().[2])
+    | "translate" -> let d = data 3 in Matrix.Translation(d.[0], d.[1], d.[2])
+    | "rotate" -> let d = data 4 in Matrix.RotationAxis(Vector3(d.[0], d.[1], d.[2]), d.[3] / 180.0f * float32 System.Math.PI)
+    | "scale" -> let d = data 3 in Matrix.Scaling(d.[0], d.[1], d.[2])
     | "matrix" ->
+        let d = data 16
         let mutable m = Matrix()
-        for i in 0..15 do m.Item(i % 4, i / 4) <- d().[i]
+        for i in 0..15 do m.Item(i % 4, i / 4) <- d.[i]
         m
     | _ -> Matrix.Identity
 
 let getNodeTransformLocal (node: XmlNode) =
-    [| for n in node.ChildNodes -> n |]
-    |> Array.rev
-    |> Array.fold (fun acc comp -> acc * getNodeTransformComponent comp) Matrix.Identity
+    Array.foldBack (fun comp acc -> acc * getNodeTransformComponent comp) [| for n in node.ChildNodes -> n |] Matrix.Identity
 
 let rec getNodeTransformAbsolute (node: XmlNode) =
     if node.Name = "node" then
