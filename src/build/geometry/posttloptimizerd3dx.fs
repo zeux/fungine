f9617f36eaf991dd8b1748c48ca21a6ddc022ff5
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2010-12-12 21:14:41 +0300

    build/geometry: Implemented vertex cache optimizer based on D3DXOptimizeFaces

---
+++ src/build/geometry/posttloptimizerd3dx.fs
@@ -0,0 +1,17 @@
+module Build.Geometry.PostTLOptimizerD3DX
+
+open System.Runtime.InteropServices
+
+[<DllImport("d3dx9_24")>]
+extern int D3DXOptimizeFaces([<MarshalAs(UnmanagedType.LPArray)>] int[] indices, int face_count, int vertex_count, bool indices32, [<MarshalAs(UnmanagedType.LPArray)>] int[] remap)
+
+let optimize indices =
+    let vertex_count = 1 + Array.max indices
+    let face_count = indices.Length / 3
+
+    let remap: int array = Array.zeroCreate face_count
+
+    let result = D3DXOptimizeFaces(indices, face_count, vertex_count, true, remap)
+    assert (result = 0)
+
+    Array.init indices.Length (fun i -> indices.[remap.[i / 3] * 3 + i % 3])
