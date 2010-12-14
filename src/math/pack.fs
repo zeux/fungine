67778953e9b8e43520ccff067c90af95bfcff3eb
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2010-12-15 01:05:16 +0300

    math: Improved direction packing functions, added unnormalized direction packing

--- src/math/pack.fs
+++ src/math/pack.fs
@@ -3,23 +3,68 @@ module Math.Pack
 let inline private clamp value left right =
     min right (max left value)
 
+let inline private round value =
+    int (value + (if value > 0.f then 0.5f else -0.5f))
+
 let packFloatUNorm value bits =
     let scale = (1 <<< bits) - 1
-    uint32 ((clamp value 0.f 1.f) * (float32 scale) + 0.5f)
+    uint32 (round ((clamp value 0.f 1.f) * (float32 scale)))
 
 let packFloatSNorm value bits =
-    let scale = (1 <<< bits) - 1
-    let signed = int ((clamp value -1.f 1.f) * (float32 scale) * 0.5f)
-    (uint32 signed) &&& (uint32 scale)
+    let scale = (1 <<< (bits - 1)) - 1
+    let mask = (1u <<< bits) - 1u
+    let result = round ((clamp value -1.f 1.f) * (float32 scale))
+    (uint32 result) &&& mask
 
-let packDirectionR8G8B8 (v: Vector3) =
-    let x = packFloatSNorm v.X 8
-    let y = packFloatSNorm v.Y 8
-    let z = packFloatSNorm v.Z 8
-    x ||| (y <<< 8) ||| (z <<< 16)
+let packDirectionSNorm (v: Vector3) bits =
+    let x = packFloatSNorm v.X bits
+    let y = packFloatSNorm v.Y bits
+    let z = packFloatSNorm v.Z bits
+    x ||| (y <<< bits) ||| (z <<< (2 * bits))
 
-let packDirectionR10G10B10 (v: Vector3) =
-    let x = packFloatUNorm (v.X * 0.5f + 0.5f) 10
-    let y = packFloatUNorm (v.Y * 0.5f + 0.5f) 10
-    let z = packFloatUNorm (v.Z * 0.5f + 0.5f) 10
-    x ||| (y <<< 10) ||| (z <<< 20)
+let packDirectionUNorm (v: Vector3) bits =
+    let x = packFloatUNorm (v.X * 0.5f + 0.5f) bits
+    let y = packFloatUNorm (v.Y * 0.5f + 0.5f) bits
+    let z = packFloatUNorm (v.Z * 0.5f + 0.5f) bits
+    x ||| (y <<< bits) ||| (z <<< (2 * bits))
+
+let packDirectionUnnormalized (v: Vector3) bits offset =
+    // round x to nearest integer
+    let inline round x = int (x + (if x > 0.f then 0.5f else -0.5f))
+
+    // get rounding error for positive x
+    let inline rounderr x = abs (x - float (int (x + 0.5)))
+
+    // normalize by largest component
+    let axis = if abs v.X > abs v.Y && abs v.X > abs v.Z then 0 else if abs v.Y > abs v.Z then 1 else 2
+    let n1 = v / abs v.[axis]
+
+    // select two other components (abs values)
+    let n1_0 = abs (float (if axis = 0 then n1.Y else n1.X))
+    let n1_1 = abs (float (if axis = 2 then n1.Y else n1.Z))
+
+    // approximate both components with integer ratios with a common denominator
+    let mutable bestv = 0.0
+    let mutable beste = infinity
+
+    for v in 1 <<< (bits - 2) .. (1 <<< (bits - 1)) - 1 do
+        let fv = float v
+        let e = ((rounderr (n1_0 * fv)) + (rounderr (n1_1 * fv))) / fv
+        
+        if e < beste then
+            beste <- e
+            bestv <- fv
+
+    // pack values together
+    let mask = (1u <<< bits) - 1u
+    let x = uint32 (round (n1.X * float32 bestv) + offset) &&& mask
+    let y = uint32 (round (n1.Y * float32 bestv) + offset) &&& mask
+    let z = uint32 (round (n1.Z * float32 bestv) + offset) &&& mask
+    
+    x ||| (y <<< bits) ||| (z <<< (2 * bits))
+
+let packDirectionUnnormalizedSigned (v: Vector3) bits =
+    packDirectionUnnormalized v bits 0
+
+let packDirectionUnnormalizedUnsigned (v: Vector3) bits =
+    packDirectionUnnormalized v bits (1 <<< (bits - 1))
