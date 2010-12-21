265daf74de893d58af3a608c85a31b60e5024634
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2010-12-21 19:28:14 +0300

    winui: PropertyTree improvements (added changed properties visualization, removed 'ref' type restriction with reflection - any type with Value, ValueChanged and DefaultValue properties will work)

--- src/winui/propertytree.fs
+++ src/winui/propertytree.fs
@@ -10,11 +10,11 @@ type private Grid with
         Grid.SetColumn(control, column)
 
 type private TreeNode =
-    | Variable of string * obj ref
+    | Variable of string * obj
     | Group of string * (TreeNode array)
 
 // filter array of (description, object) pairs with search string
-let private filter (pattern: string) (data: (string * obj ref) array) =
+let private filter (pattern: string) (data: (string * obj) array) =
     // break pattern into words
     let words = pattern.Split(" \t".ToCharArray())
 
@@ -24,7 +24,7 @@ let private filter (pattern: string) (data: (string * obj ref) array) =
         words |> Array.forall (fun w -> description.Contains(w)))
 
 // create tree nodes from an array of (path elements, object) pairs
-let rec private buildTreePath (paths: (string array * obj ref) array) =
+let rec private buildTreePath (paths: (string array * obj) array) =
     // group descriptions by the first path element and remove it
     let removeHead data = Array.sub data 1 (data.Length - 1)
     let removeHeadAll data = data |> Seq.map (fun (elements, o) -> removeHead elements, o) |> Seq.toArray
@@ -39,7 +39,7 @@ let rec private buildTreePath (paths: (string array * obj ref) array) =
         | _ -> Group(key, buildTreePath value))
 
 // create tree nodes from an array of (description, object) pairs
-let rec private buildTree (data: (string * obj ref) array) =
+let rec private buildTree (data: (string * obj) array) =
     // split descriptions with separator
     let paths = data |> Array.map (fun (description, o) -> description.Split('/'), o)
 
@@ -56,11 +56,15 @@ let private createHorizontalGrid controls =
 
     grid
 
+// property accessors
+let private (?) data key = data.GetType().GetProperty(key).GetValue(data, null)
+let private (?<-) data key value = data.GetType().GetProperty(key).SetValue(data, value, null)
+
 // typed value proxy for text box
-type StringAccessor<'T>(data: obj ref, conv: string -> 'T) =
+type StringAccessor<'T>(data: obj, conv: string -> 'T) =
     member x.Value
-        with get () = string (!data)
-        and set value = data := box (conv value)
+        with get () = string (data?Value)
+        and set value = data?Value <- box (conv value)
     
 // create named control group
 let private createControlGroup name (control: 'a) =
@@ -79,8 +83,8 @@ let private createControlEdit name data =
     createControlGroup name edit
 
 // create control from variable reference
-let private createControl name (data: obj ref) =
-    match !data with
+let private createControl name (data: obj) =
+    match data?Value with
     | :? bool ->
         // check box bound to data
         let cb = CheckBox(Content = name)
@@ -105,24 +109,32 @@ let rec private buildTreeViewItems nodes =
     nodes |> Seq.map (fun node ->
         match node with
         | Variable (name, data) ->
-            let c = createControl name data
-            TreeViewItem(Header = c, Margin = Thickness(0.0, 1.0, 0.0, 2.0))
+            // item with background color bound to data changed state
+            let item = TreeViewItem(Header = createControl name data, Padding = Thickness(1.0), Margin = Thickness(1.0))
+            (data?ValueChanged :?> IEvent<_>).Add(fun _ -> item.Background <- if data?DefaultValue = data?Value then Media.Brushes.Transparent else Media.Brushes.Bisque)
+
+            item
         | Group (name, children) ->
             TreeViewItem(Header = name, IsExpanded = true, ItemsSource = buildTreeViewItems children))
 
+// build tree view items with cache
+let private buildTreeView (tree: TreeView) variables =
+    if tree.Tag <> box variables then
+        tree.ItemsSource <- variables |> buildTree |> buildTreeViewItems
+        tree.Tag <- variables
+
 // create window from variables
 let create variables =
-    let build pattern = variables |> filter pattern |> buildTree |> buildTreeViewItems
-
     // create tree
-    let tree = TreeView(ItemsSource = build "")
+    let tree = TreeView()
+    buildTreeView tree variables
 
     VirtualizingStackPanel.SetIsVirtualizing(tree, true)
 
     // create filter box
     let filter_box = TextBox()
 
-    filter_box.TextChanged.Add(fun _ -> tree.ItemsSource <- build filter_box.Text)
+    filter_box.TextChanged.Add(fun _ -> buildTreeView tree (variables |> filter filter_box.Text))
 
     // add filter box and tree to grid
     let grid = Grid()
