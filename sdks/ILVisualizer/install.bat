4eafa7c336b536ce1a6e78551abab906a173f9a9
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2011-01-07 08:46:41 +0300

    sdks: Added ILVisualizer (debugger visualizer for dynamic methods)

---
+++ sdks/ILVisualizer/install.bat
@@ -0,0 +1,9 @@
+@echo off
+setlocal
+
+rem get 'my documents' path
+for /f "tokens=2* skip=2" %%i in ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v personal') do set DOCUMENTS=%%j
+for /f "tokens=*" %%i in ('echo %DOCUMENTS%') do set DOCUMENTS=%%i
+
+rem copy visualizer files to Visual Studio path
+copy *.dll "%DOCUMENTS%\Visual Studio 2010\Visualizers\"
