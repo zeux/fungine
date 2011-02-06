14fa150b6aa915fd04d62d48e9937c5c8bb5a284
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2011-02-06 21:53:00 +0300

    core: Added access to value collection in Cache (is it thread-safe?)

--- src/core/cache.fs
+++ src/core/cache.fs
@@ -6,6 +6,9 @@ open System.Collections.Concurrent
 type Cache<'K, 'V when 'K: equality>(creator) =
     let cache = ConcurrentDictionary<'K, 'V>(HashIdentity.Structural)
 
+    // get the cached values
+    member this.Values = cache.Values
+
     // get the value from the cache, creating it as necessary
     member this.Get key =
         cache.GetOrAdd(key, fun key -> creator key)
