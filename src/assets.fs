d933505fa4f1abf63ad867fbb881aa902bbb231a
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2011-03-17 23:26:44 -0700

    build/dae: Introduced MeshBuilder (builds intermediate structures for now)

--- src/assets.fs
+++ src/assets.fs
@@ -1,11 +1,5 @@
 ﻿module assets
 
-open System.Collections.Generic
-
-open Build.Geometry
-open Build.Dae
-open Build.Dae.Parse
-
 let build source target func =
     let source_info = System.IO.FileInfo(source)
     let target_info = System.IO.FileInfo(target)
@@ -19,64 +13,22 @@ let build source target func =
 let changeExtension name ext =
     System.IO.Path.ChangeExtension(name, ext)
 
-let buildMeshImpl source target =
+let buildMesh source =
     // export .dae file
     let dae = ".build/" + changeExtension source ".dae"
-    build source dae Export.build
 
-    // parse .dae file
-    let doc = Document(dae)
+    build source dae Build.Dae.Export.build
 
-    // get cached texture/material builders
-    let all_textures = Core.Cache (fun id -> Build.Dae.TextureBuilder.build doc id)
-    let all_materials = Core.Cache (fun id -> Build.Dae.MaterialBuilder.build doc id (all_textures.Get >> snd))
-
-    // get basis converter (converts up axis and units)
-    let conv = BasisConverter(doc, 1.f)
-
-    // export skeleton
-    let skeleton = SkeletonBuilder.build doc conv
-
-    // use a constant FVF for now
-    let fvf = [|Position; Tangent; Bitangent; Normal; TexCoord 0; SkinningInfo 4|]
-    let format = Render.VertexFormat.Pos_TBN_Tex1_Bone4_Packed
-
-    // export meshes
-    let instances = doc.Root.Select("/COLLADA/library_visual_scenes//node/instance_geometry | /COLLADA/library_visual_scenes//node/instance_controller")
-
-    let meshes = instances |> Array.collect (fun i ->
-        // build fat meshes from .dae
-        let fat_meshes = FatMeshBuilder.build doc conv i fvf skeleton
-
-        // build materials
-        let materials = fat_meshes |> Array.map (fun (_, material) -> all_materials.Get material)
-
-        // build packed & indexed meshes
-        let packed_meshes = fat_meshes |> Array.map (fun (mesh, _) -> Build.Geometry.MeshPacker.pack mesh format)
-
-        // optimize for Post T&L cache
-        let postopt_meshes = packed_meshes |> Array.map (fun mesh -> { mesh with indices = Build.Geometry.PostTLOptimizerLinear.optimize mesh.indices })
-
-        // optimize for Pre T&L cache
-        let preopt_meshes = postopt_meshes |> Array.map (fun mesh ->
-            let (vertices, indices) = Build.Geometry.PreTLOptimizer.optimize mesh.vertices mesh.indices mesh.vertex_size
-            { mesh with vertices = vertices; indices = indices })
-
-        Array.map2 (fun mesh material -> mesh, material, skeleton.data, skeleton.data.AbsoluteTransform skeleton.node_map.[i.ParentNode]) preopt_meshes materials)
-
-    Core.Serialization.Save.toFile target meshes
-
-    // export textures
-    all_textures.Pairs |> Seq.iter (fun p -> let (source, tex) = p.Value in build source tex.Path Build.Texture.build)
-
-    true
-
-let buildMesh source =
+    // export .mesh file
     let target = ".build/" + changeExtension source ".mesh"
 
-    build source target buildMeshImpl
+    build dae target (fun source target ->
+        let textures = Build.Dae.MeshBuilder.build source target
+        textures |> Seq.iter (fun (source, tex) -> build source tex.Path Build.Texture.build)
+        true)
 
-    (Core.Serialization.Load.fromFile target) :?> (PackedMesh * Render.Material * Render.SkeletonInstance * Matrix34) array
+    // load .mesh file
+    (Core.Serialization.Load.fromFile target) :?> (Build.Geometry.PackedMesh * Render.Material * Render.SkeletonInstance * Matrix34) array
     
 let buildMeshes path =
     let patterns = [|"*.mb"; "*.ma"; "*.max"|]
---
+++ src/build/dae/meshbuilder.fs
@@ -0,0 +1,50 @@
+﻿module Build.Dae.MeshBuilder
+
+open Build.Geometry
+open Build.Dae.Parse
+
+let build source target =
+    // parse .dae file
+    let doc = Document(source)
+
+    // get cached texture/material builders
+    let all_textures = Core.Cache (fun id -> TextureBuilder.build doc id)
+    let all_materials = Core.Cache (fun id -> MaterialBuilder.build doc id (all_textures.Get >> snd))
+
+    // get basis converter (converts up axis and units)
+    let conv = BasisConverter(doc, 1.f)
+
+    // export skeleton
+    let skeleton = SkeletonBuilder.build doc conv
+
+    // use a constant FVF for now
+    let fvf = [|Position; Tangent; Bitangent; Normal; TexCoord 0; SkinningInfo 4|]
+    let format = Render.VertexFormat.Pos_TBN_Tex1_Bone4_Packed
+
+    // export meshes
+    let instances = doc.Root.Select("/COLLADA/library_visual_scenes//node/instance_geometry | /COLLADA/library_visual_scenes//node/instance_controller")
+
+    let meshes = instances |> Array.collect (fun i ->
+        // build fat meshes from .dae
+        let fat_meshes = FatMeshBuilder.build doc conv i fvf skeleton
+
+        // build materials
+        let materials = fat_meshes |> Array.map (fun (_, material) -> all_materials.Get material)
+
+        // build packed & indexed meshes
+        let packed_meshes = fat_meshes |> Array.map (fun (mesh, _) -> MeshPacker.pack mesh format)
+
+        // optimize for Post T&L cache
+        let postopt_meshes = packed_meshes |> Array.map (fun mesh -> { mesh with indices = PostTLOptimizerLinear.optimize mesh.indices })
+
+        // optimize for Pre T&L cache
+        let preopt_meshes = postopt_meshes |> Array.map (fun mesh ->
+            let (vertices, indices) = PreTLOptimizer.optimize mesh.vertices mesh.indices mesh.vertex_size
+            { mesh with vertices = vertices; indices = indices })
+
+        Array.map2 (fun mesh material -> mesh, material, skeleton.data, skeleton.data.AbsoluteTransform skeleton.node_map.[i.ParentNode]) preopt_meshes materials)
+
+    Core.Serialization.Save.toFile target meshes
+
+    // return texture list
+    all_textures.Pairs |> Seq.map (fun p -> p.Value)
