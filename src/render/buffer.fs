f2dd20f586871870c68efa989f41a42ad06963a1
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2011-03-17 23:16:29 -0700

    render: Introduced buffer object helper (for serialization)

---
+++ src/render/buffer.fs
@@ -0,0 +1,15 @@
+namespace Render
+
+open SlimDX.Direct3D11
+
+// vertex/index buffer
+type Buffer(bind_flags, contents: byte array) =
+    // buffer object
+    [<System.NonSerialized>]
+    let mutable data = null
+
+    // lazy initializing resource accessor ($$ replace with post-serialization callback)
+    member private this.Resource =
+        if data = null then
+            data <- new SlimDX.Direct3D11.Buffer(Render.Device.get(), BufferDescription(SizeInBytes = contents.Length, BindFlags = bind_flags))
+        data
