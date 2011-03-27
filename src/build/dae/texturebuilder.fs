0acf5b7f971e18a72d3ab95d9760fb0c3134ad8f
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2011-03-27 16:43:34 -0700

    build/dae: Minor refactoring

--- src/build/dae/texturebuilder.fs
+++ src/build/dae/texturebuilder.fs
@@ -1,27 +1,26 @@
-﻿namespace Build.Dae
+﻿module Build.Dae.TextureBuilder
 
 open System
 open System.IO
 
 open Build.Dae.Parse
 
-module TextureBuilder =
-    // get relative path
-    let private relativePath path root =
-        Uri(root).MakeRelativeUri(Uri(path)).OriginalString
+// get relative path
+let private relativePath path root =
+    Uri(root).MakeRelativeUri(Uri(path)).OriginalString
 
-    // get texture path from id
-    let build (doc: Document) id =
-        // get image node
-        let image = doc.Node id
+// get texture path from id
+let build (doc: Document) id =
+    // get image node
+    let image = doc.Node id
 
-        // get image path
-        let path = image.SelectSingleNode("init_from/text()").Value
-        let relative_path = relativePath path (Environment.CurrentDirectory + "/")
-        if Path.IsPathRooted(relative_path) || relative_path.StartsWith("../") then failwithf "Out-of-source texture paths are not supported: %s" path
+    // get image path
+    let path = image.SelectSingleNode("init_from/text()").Value
+    let relative_path = relativePath path (Environment.CurrentDirectory + "/")
+    if Path.IsPathRooted(relative_path) || relative_path.StartsWith("../") then failwithf "Out-of-source texture paths are not supported: %s" path
 
-        // build texture object
-        let source = relative_path
-        let target = ".build/" + Path.ChangeExtension(relative_path, ".dds")
+    // build texture object
+    let source = relative_path
+    let target = ".build/" + Path.ChangeExtension(relative_path, ".dds")
 
-        source, Render.Texture(target)
+    source, Render.Texture(target)
