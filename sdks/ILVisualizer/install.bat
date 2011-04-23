78828b85cba26ce9574ccecce61fda342eb88ba9
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2011-04-22 19:45:40 -0700

    sdks/ILVisualizer: Comment fix

--- sdks/ILVisualizer/install.bat
+++ sdks/ILVisualizer/install.bat
@@ -1,9 +1,9 @@
 @echo off
 setlocal
 
-rem get 'my documents' path
+rem Get 'my documents' path
 for /f "tokens=2* skip=2" %%i in ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v personal') do set DOCUMENTS=%%j
 for /f "tokens=*" %%i in ('echo %DOCUMENTS%') do set DOCUMENTS=%%i
 
-rem copy visualizer files to Visual Studio path
+rem Copy visualizer files to Visual Studio path
 copy *.dll "%DOCUMENTS%\Visual Studio 2010\Visualizers\"
