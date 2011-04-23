caf028edbb6d7d571394a13d50e0463fbaeaee51
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2011-04-22 22:28:29 -0700

    sdks/SlimDX: Updated SlimDX to r2067 (March 2011), slightly reorganized the diff (custom project file instead of patched original), added command-line build

---
+++ sdks/SlimDX/build.bat
@@ -0,0 +1,22 @@
+@echo off
+setlocal
+
+rem Checkout sources
+call svn checkout http://slimdx.googlecode.com/svn/trunk/ slimdx
+
+rem Apply patch:
+rem 1. All components except D3D11-related ones and RawInput are stripped
+rem 2. Resources and custom exception error messages (dxerr.lib) are stripped
+rem 3. ObjectTable is removed, all objects now have finalizers (no need to manually dispose everything)
+patch -p0 -d slimdx <SlimDX.diff
+
+rem Build solution
+for /f "usebackq tokens=3" %%i in (`reg query HKLM\Software\Microsoft\MSBuild\ToolsVersions\4.0 /v MSBuildToolsPath`) do set MSBUILDPATH=%%i
+%MSBUILDPATH%\msbuild SlimDX.sln /nologo /verbosity:quiet /p:configuration=Release /p:platform=Win32 || exit %ERRORLEVEL%
+
+rem Copy results
+copy /y Release\SlimDX.dll SlimDX.dll
+
+rem Delete artefacts
+rmdir /s /q Release
+rmdir /s /q SlimDX
