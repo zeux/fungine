6cbc39e143d3e5851550086176ec4203f206846d
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2011-04-30 19:31:57 -0700

    sdks/nvtt: Removed VS2010 patch (it was applied to nvtt trunk)

--- sdks/nvtt/build.bat
+++ sdks/nvtt/build.bat
@@ -4,9 +4,6 @@ setlocal
 rem Checkout sources
 call svn checkout http://nvidia-texture-tools.googlecode.com/svn/trunk/ nvtt
 
-rem Apply VS2010 patch
-patch -p0 -d nvtt <nvtt.diff
-
 rem Build solution
 for /f "usebackq tokens=3" %%i in (`reg query HKLM\Software\Microsoft\MSBuild\ToolsVersions\4.0 /v MSBuildToolsPath`) do set MSBUILDPATH=%%i
 %MSBUILDPATH%\msbuild nvtt.sln /nologo /verbosity:quiet /p:configuration=Release /p:platform=Win32 || exit %ERRORLEVEL%
