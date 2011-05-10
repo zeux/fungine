a1473c400bf7e4eacfda00af74c7220b7dd65410
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2011-05-09 23:15:43 -0700

    build/system: Optimized Signature.Combine

--- src/build/system/signature.fs
+++ src/build/system/signature.fs
@@ -18,6 +18,10 @@ type Signature(high: uint64, low: uint64) =
         let extract64 offset = BitConverter.ToInt64(data, offset) |> IPAddress.HostToNetworkOrder |> uint64
         Signature(extract64 0, extract64 8)
 
+    // high/low part accessors
+    member this.ValueHigh = high
+    member this.ValueLow = low
+
     // compute signature from stream
     static member FromStream (data: Stream) = Signature(generator.ComputeHash(data))
 
@@ -33,7 +37,13 @@ type Signature(high: uint64, low: uint64) =
     static member FromString (data: string) = Signature.FromBytes(encoding.GetBytes(data))
 
     // combine several signatures into one
-    static member Combine (list: Signature seq) = list |> Seq.map string |> Seq.reduce (+) |> Signature.FromString
+    static member Combine (list: Signature seq) =
+        list
+        |> Seq.toArray
+        |> Array.collect (fun s ->
+            let convert64 (value: uint64) = value |> int64 |> IPAddress.NetworkToHostOrder |> BitConverter.GetBytes
+            Array.append (convert64 s.ValueHigh) (convert64 s.ValueLow))
+        |> Signature.FromBytes
         
     // string conversion
     override this.ToString() = sprintf "%08x%08x" high low
