e9b382cc4f062c56450a7eb0115a89eeeae7fcec
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2011-05-16 22:39:24 -0700

    sandbox: Replaced getTarget with Context.Target

--- src/sandbox/assets.fs
+++ src/sandbox/assets.fs
@@ -7,17 +7,14 @@ open System.IO
 
 let context = Context(System.Environment.CurrentDirectory, ".build")
 
-// create target name
-let getTarget (source: Node) ext = Node (Path.Combine(context.BuildPath, Path.ChangeExtension(source.Path, ext)))
-
 // mesh export
 let Mesh path =
     // export .dae file
-    let dae = getTarget path ".dae"
+    let dae = context.Target path ".dae"
     context.Task(Dae.Export.builder, source = path, target = dae)
 
     // export .mesh file
-    let mesh = getTarget path ".mesh"
+    let mesh = context.Target path ".mesh"
     context.Task(Dae.MeshBuilder.builder, source = dae, target = mesh)
 
 let buildMeshes path =
