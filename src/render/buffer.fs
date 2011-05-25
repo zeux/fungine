83e830c6fb0f97a2093c2e41cb5e5b53a347fa4e
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2011-05-24 21:03:23 -0700

    render/buffer: Replaced lazy resource initialization with fixup callback

--- src/render/buffer.fs
+++ src/render/buffer.fs
@@ -8,9 +8,10 @@ type Buffer(bind_flags, contents: byte array) =
     [<System.NonSerialized>]
     let mutable data = null
 
-    // lazy initializing resource accessor ($$ replace with post-serialization callback)
-    member this.Resource =
-        if data = null then
-            use stream = new SlimDX.DataStream(contents, canRead = true, canWrite = false)
-            data <- new SlimDX.Direct3D11.Buffer(Render.Device.get(), stream, BufferDescription(SizeInBytes = contents.Length, BindFlags = bind_flags))
-        data
+    // fixup callback
+    member private this.Fixup device =
+        use stream = new SlimDX.DataStream(contents, canRead = true, canWrite = false)
+        data <- new SlimDX.Direct3D11.Buffer(device, stream, BufferDescription(SizeInBytes = contents.Length, BindFlags = bind_flags))
+
+    // resource accessor
+    member this.Resource = data
