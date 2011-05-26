e54fb525e7d489b7d69e420a85b9b6e98f53aa89
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2011-05-25 23:04:52 -0700

    render: Removed now redundant device singleton

--- src/render/device.fs
+++ src/render/device.fs
@@ -1,14 +1,1 @@
 module Render.Device
-
-// device singleton
-let mutable private device: SlimDX.Direct3D11.Device = null
-
-// get device
-let get () =
-    assert (device <> null)
-    device
-
-// set device
-let set value =
-    assert (device = null && value <> null)
-    device <- value
