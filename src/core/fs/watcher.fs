87cc3f4b3df50840bce232d3570b3b0464006de4
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2011-06-09 22:59:58 -0700

    core/fs: Change watcher now properly retries files that could not be read when notification arrived

--- src/core/fs/watcher.fs
+++ src/core/fs/watcher.fs
@@ -8,7 +8,7 @@ open Microsoft.Win32.SafeHandles
 // file change watcher
 type Watcher(path, callback) =
     // native createfile to avoid exceptions when checking for file read
-    [<DllImport("kernel32", CharSet = CharSet.Unicode)>]
+    [<DllImport("kernel32", CharSet = CharSet.Unicode, SetLastError = true)>]
     static extern SafeFileHandle CreateFile(string lpFileName, int dwDesiredAccess, FileShare dwShareMode, nativeint securityAttrs, FileMode dwCreationDisposition, int dwFlagsAndAttributes, nativeint hTemplateFile);
 
     // watcher object
@@ -23,6 +23,14 @@ type Watcher(path, callback) =
 
             // skip files that can't be read (some notifications arrive while the file is being written by the other process)
             if (use handle = CreateFile(msg, (* FILE_READ_DATA *) 1, FileShare.Read, 0n, FileMode.Open, 0, 0n) in handle.IsInvalid) then
+                if Marshal.GetLastWin32Error() = (* ERROR_SHARING_VIOLATION *) 32 then
+                    // another process is writing the file, retry with some delay
+                    let! _ =
+                        async {
+                            do! Async.Sleep(50)
+                            inbox.Post(msg)
+                        } |> Async.StartChild
+                    ()
                 return! loop ()
 
             // get normalized path & modtime
