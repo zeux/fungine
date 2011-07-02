eebfa079836baf0cf28c5ea244081bb36c662658
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2011-07-02 16:07:14 +0400

    build/shader: Implemented include file handling (without dependencies)

--- src/build/shader/shader.fs
+++ src/build/shader/shader.fs
@@ -4,16 +4,39 @@ open BuildSystem
 
 open SlimDX
 open SlimDX.D3DCompiler
+open System.IO
 
 // get byte array from data stream
 let private getData (stream: DataStream) =
     stream.Position <- 0L
     stream.ReadRange<byte>(int stream.Length)
 
+// include handler
+type private IncludeHandler(root_file) =
+    interface Include with
+        // open included file
+        override this.Open(typ, file, parent, stream) =
+            // get parent file name
+            let parent_file =
+                match parent with
+                | null -> root_file
+                | :? FileStream as f -> f.Name
+                | _ -> failwithf "Internal error: unknown stream %A" parent
+
+            // make full file name
+            let path = Path.Combine(Path.GetDirectoryName(parent_file), file)
+
+            // open file
+            stream <- try File.OpenRead(path) with e -> null
+
+        // close included file
+        override this.Close(stream) =
+            stream.Dispose()
+
 // build single bytecode instance
 let private buildBytecode path entry profile =
     let flags = ShaderFlags.PackMatrixRowMajor ||| ShaderFlags.WarningsAreErrors
-    ShaderBytecode.CompileFromFile(path, entry, profile, flags, EffectFlags.None)
+    ShaderBytecode.CompileFromFile(path, entry, profile, flags, EffectFlags.None, [||], IncludeHandler(path))
 
 // build shader
 let private build source target version =
