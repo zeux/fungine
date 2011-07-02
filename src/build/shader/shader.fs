b106a5ab3f0c5b120b117007b049d1df6eaa6685
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2011-07-02 15:42:09 +0400

    build/shader: Initial shader builder implementation (no include dependencies, single vs/ps pair)

---
+++ src/build/shader/shader.fs
@@ -0,0 +1,35 @@
+module Build.Shader
+
+open BuildSystem
+
+open SlimDX
+open SlimDX.D3DCompiler
+
+// get byte array from data stream
+let private getData (stream: DataStream) =
+    stream.Position <- 0L
+    stream.ReadRange<byte>(int stream.Length)
+
+// build single bytecode instance
+let private buildBytecode path entry profile =
+    let flags = ShaderFlags.PackMatrixRowMajor ||| ShaderFlags.WarningsAreErrors
+    ShaderBytecode.CompileFromFile(path, entry, profile, flags, EffectFlags.None)
+
+// build shader
+let private build source target version =
+    let vs = buildBytecode source "vs_main" ("vs_" + version)
+    let vssig = ShaderSignature.GetInputSignature(vs)
+
+    let ps = buildBytecode source "ps_main" ("ps_" + version)
+
+    let shader =
+        Render.Shader(
+            vertex_signature = Render.ShaderSignature(getData vssig.Data),
+            vertex = Render.ShaderObject(getData vs.Data),
+            pixel = Render.ShaderObject(getData ps.Data))
+
+    Core.Serialization.Save.toFile target shader
+
+// shader builder object
+let builder = ActionBuilder("Shader", fun task ->
+    build task.Sources.[0].Path task.Targets.[0].Path "5_0")
