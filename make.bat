e4303179bd41cf8198d80107f7c7aa22f2d5b1a6
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2011-07-04 11:28:49 +0400

    Fixed make.bat launch path

--- make.bat
+++ make.bat
@@ -5,4 +5,4 @@ set _OPTIONS_PLATFORM=x86
 for /f "usebackq tokens=3" %%i in (`reg query HKLM\Software\Microsoft\MSBuild\ToolsVersions\4.0 /v MSBuildToolsPath`) do set MSBUILDPATH=%%i
 %MSBUILDPATH%\msbuild src/fungine.sln /nologo /verbosity:quiet /p:configuration=%_OPTIONS_CONFIG% /p:platform=%_OPTIONS_PLATFORM% || exit %ERRORLEVEL%
 
-bin\%_OPTIONS_CONFIG%_%_OPTIONS_PLATFORM%\fungine
+bin\%_OPTIONS_CONFIG%_%_OPTIONS_PLATFORM%\sandbox
