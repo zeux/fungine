204485e75d0e16f933477c3b175d9b7dc060bf24
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2011-08-16 23:48:03 -0700

    shaders: Fixed gamma/degamma function names

--- src/shaders/common/gbuffer.h
+++ src/shaders/common/gbuffer.h
@@ -24,11 +24,11 @@ Surface gbufSampleSurface(float2 uv)
 {
     Surface O;
 
-    O.albedo = gamma(gbuf_albedo.Sample(gbuf_sampler, uv)).rgb;
+    O.albedo = degamma(gbuf_albedo.Sample(gbuf_sampler, uv)).rgb;
     O.normal = gbuf_normal.Sample(gbuf_sampler, uv).xyz * 2 - 1;
 
     float4 spec = gbuf_specular.Sample(gbuf_sampler, uv);
-    O.specular = gamma(spec.rgb);
+    O.specular = degamma(spec.rgb);
     O.roughness = spec.a;
 
     O.depth = gbuf_depth.Sample(gbuf_sampler, uv).r;
