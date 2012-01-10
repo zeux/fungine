3131426280bae0e17abf511383157e8e7dcbdcab
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2012-01-09 23:29:18 -0800

    shaders/lighting: Implemented several shadow filtering functions (PCF, Poisson, FXAA)

--- src/shaders/lighting/spot.hlsl
+++ src/shaders/lighting/spot.hlsl
@@ -28,17 +28,86 @@ PS_IN vsMain(uint id: SV_VertexID)
     return O;
 }
 
-float SampleShadow(float2 uv, float depth)
+float SampleShadow(float2 uv, float depth, int2 offset = 0)
 {
+    return shadowMap.SampleCmpLevelZero(shadowSampler, uv, depth, offset);
+}
+
+float2 GetShadowMapSize()
+{
+    float2 result;
+    shadowMap.GetDimensions(result.x, result.y);
+
+    return result;
+}
+
+float SampleShadowPCF(float2 uv, float depth, int size)
+{
+    int radius = size / 2;
+    int area = (radius * 2 + 1) * (radius * 2 + 1);
+
     float result = 0;
     
-    [unroll] for (int x = -1; x <= 1; ++x)
-        [unroll] for (int y = -1; y <= 1; ++y)
+    [unroll] for (int x = -radius; x <= radius; ++x)
+        [unroll] for (int y = -radius; y <= radius; ++y)
         {
-            result += shadowMap.SampleCmpLevelZero(shadowSampler, uv, depth, int2(x, y));
+            result += SampleShadow(uv, depth, int2(x, y));
         }
 
-    return result / 9;
+    return result / area;
+}
+
+float SampleShadowPoisson(float2 uv, float depth, int size)
+{
+    const float2 offsets[13] =
+    {
+        {0, 0},
+        {0.200887527842703, -0.805816066868008}, {0.169759583602972, 0.787268282932537}, {-0.639597778815228, -0.370236979450183},
+        {0.629148098269191, 0.367398185340504}, {-0.456211403483755, 0.542374725109481}, {-0.411828977295713, -0.484566120009967},
+        {0.106871055317844, 0.59918690478536}, {-0.117388437710382, -0.518626519610864}, {0.436862373204161, -0.182937652849816},
+        {0.206632546759667, 0.343668469981935}, {-0.330409446933952, 0.175773621457362}, {-0.1, -0.16089955706328},
+    };
+
+    float2 scale = 1 / GetShadowMapSize();
+
+    float result = 0;
+
+    [unroll] for (int i = 0; i < size; ++i)
+    {
+        result += SampleShadow(uv + offsets[i] * scale, depth);
+    }
+
+    return result / size;
+}
+
+float SampleShadowFXAA(float2 uv, float depth)
+{
+    float2 scale = 1 / GetShadowMapSize();
+
+    float shadowM = SampleShadow(uv, depth);
+    float shadowNW = SampleShadow(uv, depth, int2(-1, -1));
+    float shadowNE = SampleShadow(uv, depth, int2(+1, -1));
+    float shadowSW = SampleShadow(uv, depth, int2(-1, +1));
+    float shadowSE = SampleShadow(uv, depth, int2(+1, +1));
+
+    float shadowDiag1 = shadowSW - shadowNE;
+    float shadowDiag2 = shadowSE - shadowNW;
+
+    float2 dir = float2(shadowDiag1 + shadowDiag2, shadowDiag1 - shadowDiag2);
+
+    if (dot(dir, dir) < 0.01) return shadowM;
+
+    float2 offset = normalize(dir) * scale;
+
+    float shadowD1 = SampleShadow(uv - offset, depth);
+    float shadowD2 = SampleShadow(uv + offset, depth);
+
+    return shadowM * 0.5 + shadowD1 * 0.25 + shadowD2 * 0.25;
+}
+
+float SampleShadowFiltered(float2 uv, float depth)
+{
+    return SampleShadowFXAA(uv, depth);
 }
 
 float4 psMain(PS_IN I): SV_Target
@@ -67,7 +136,7 @@ float4 psMain(PS_IN I): SV_Target
     // Shadow
     float4 posLsH = mul(lightCamera.viewProjection, float4(posWs, 1));
     float3 posLs = posLsH.xyz / posLsH.w;
-    float shadow = SampleShadow(posLs.xy * float2(0.5, -0.5) + 0.5,  posLs.z - 1e-5);
+    float shadow = SampleShadowFiltered(posLs.xy * float2(0.5, -0.5) + 0.5,  posLs.z - 1e-5);
 
 	return float4(shadow * light.color * (S.albedo * diffuse + specular * diffuse), 1);
 }
