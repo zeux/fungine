446d305523d8f14190a9782a5b9c2fbf7fdd80ff
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2012-05-09 00:46:28 -0700

    shaders/lighting: Implemented shadow support

--- src/shaders/lighting/integrate.h
+++ src/shaders/lighting/integrate.h
@@ -1,6 +1,8 @@
 #ifndef LIGHTING_INTEGRATE_H
 #define LIGHTING_INTEGRATE_H
 
+#include <lighting/shadowmap.h>
+
 #include <auto_LightData.h>
 #include <auto_LightGrid.h>
 
@@ -14,6 +16,16 @@ struct LightInput
     float3 color;
 };
 
+float getLightShadow(LightData light, float3 position)
+{
+    float4 p = mul(light.shadowData.transform, float4(position, 1));
+    p.xyz /= p.w;
+    p.xy = saturate(p.xy * float2(0.5, -0.5) + 0.5);
+    p.xy = p.xy * light.shadowData.atlasScale + light.shadowData.atlasOffset;
+
+    return sampleShadowFiltered(p.xy, p.z - 1e-6);
+}
+
 LightInput getLightInput(LightData light, float3 position)
 {
     float3 lightUn = light.position - position;
@@ -23,10 +35,12 @@ LightInput getLightInput(LightData light, float3 position)
     float attenCone = pow(saturate((dot(-L, light.direction) - light.outerAngle) / (light.innerAngle - light.outerAngle)), 4);
 
     float atten = (light.type == LIGHTTYPE_DIRECTIONAL ? 1 : attenDist) * (light.type == LIGHTTYPE_SPOT ? attenCone : 1);
+    
+    float shadow = getLightShadow(light, position);
 
     LightInput result;
     result.direction = light.type == LIGHTTYPE_DIRECTIONAL ? -light.direction : L;
-    result.color = light.color.rgb * (light.intensity * atten);
+    result.color = light.color.rgb * (light.intensity * atten * shadow);
 
     return result;
 }
---
+++ src/shaders/lighting/shadowmap.h
@@ -0,0 +1,89 @@
+#ifndef LIGHTING_SHADOWMAP_H
+#define LIGHTING_SHADOWMAP_H
+
+Texture2D<float> shadowMap;
+SamplerComparisonState shadowSampler;
+
+float sampleShadow(float2 uv, float depth, int2 offset = 0)
+{
+    return shadowMap.SampleCmpLevelZero(shadowSampler, uv, depth, offset);
+}
+
+float2 getShadowMapSize()
+{
+    float2 result;
+    shadowMap.GetDimensions(result.x, result.y);
+
+    return result;
+}
+
+float sampleShadowPCF(float2 uv, float depth, int size)
+{
+    int radius = size / 2;
+    int area = (radius * 2 + 1) * (radius * 2 + 1);
+
+    float result = 0;
+    
+    [unroll] for (int x = -radius; x <= radius; ++x)
+        [unroll] for (int y = -radius; y <= radius; ++y)
+        {
+            result += sampleShadow(uv, depth, int2(x, y));
+        }
+
+    return result / area;
+}
+
+float sampleShadowPoisson(float2 uv, float depth, int size)
+{
+    const float2 offsets[13] =
+    {
+        {0, 0},
+        {0.200887527842703, -0.805816066868008}, {0.169759583602972, 0.787268282932537}, {-0.639597778815228, -0.370236979450183},
+        {0.629148098269191, 0.367398185340504}, {-0.456211403483755, 0.542374725109481}, {-0.411828977295713, -0.484566120009967},
+        {0.106871055317844, 0.59918690478536}, {-0.117388437710382, -0.518626519610864}, {0.436862373204161, -0.182937652849816},
+        {0.206632546759667, 0.343668469981935}, {-0.330409446933952, 0.175773621457362}, {-0.1, -0.16089955706328},
+    };
+
+    float2 scale = 1 / getShadowMapSize();
+
+    float result = 0;
+
+    [unroll] for (int i = 0; i < size; ++i)
+    {
+        result += sampleShadow(uv + offsets[i] * scale, depth);
+    }
+
+    return result / size;
+}
+
+float sampleShadowFXAA(float2 uv, float depth)
+{
+    float2 scale = 1 / getShadowMapSize();
+
+    float shadowM = sampleShadow(uv, depth);
+    float shadowNW = sampleShadow(uv, depth, int2(-1, -1));
+    float shadowNE = sampleShadow(uv, depth, int2(+1, -1));
+    float shadowSW = sampleShadow(uv, depth, int2(-1, +1));
+    float shadowSE = sampleShadow(uv, depth, int2(+1, +1));
+
+    float shadowDiag1 = shadowSW - shadowNE;
+    float shadowDiag2 = shadowSE - shadowNW;
+
+    float2 dir = float2(shadowDiag1 + shadowDiag2, shadowDiag1 - shadowDiag2);
+
+    if (dot(dir, dir) < 0.01) return shadowM;
+
+    float2 offset = normalize(dir) * scale;
+
+    float shadowD1 = sampleShadow(uv - offset, depth);
+    float shadowD2 = sampleShadow(uv + offset, depth);
+
+    return shadowM * 0.5 + shadowD1 * 0.25 + shadowD2 * 0.25;
+}
+
+float sampleShadowFiltered(float2 uv, float depth)
+{
+    return sampleShadowFXAA(uv, depth);
+}
+
+#endif
