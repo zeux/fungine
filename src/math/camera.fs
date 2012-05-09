879f12df7f72bba4bf75b7cb4be372b24b5f414d
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2012-05-09 01:16:39 -0700

    math/camera: Added orthographic projection matrix functions

--- src/math/camera.fs
+++ src/math/camera.fs
@@ -1,5 +1,22 @@
 module Math.Camera
 
+// get an orthographic projection matrix
+let projectionOrthoOffCenter left right bottom top znear zfar =
+    let xs = 2.f / (right - left)
+    let ys = 2.f / (top - bottom)
+    let zs = 1.f / (zfar - znear)
+
+    Matrix44(xs,  0.f, 0.f, -xs * (left + right),
+             0.f, ys,  0.f, -ys * (top + bottom),
+             0.f, 0.f, zs,  -zs * znear,
+             0.f, 0.f, 0.f, 1.f)
+
+// get a symmetrical orthographic projection matrix
+let projectionOrtho width height znear zfar =
+    let hw = width / 2.f
+    let hh = height / 2.f
+    projectionOrthoOffCenter -hw hw -hh hh znear zfar
+
 // get a perspective projection matrix
 let projectionPerspective fovY aspect znear zfar =
     let ys = 1.f / tan (fovY / 2.f)
